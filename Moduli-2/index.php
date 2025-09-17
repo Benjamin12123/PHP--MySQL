f06022747493845f2cd321cf5642e56138d8049d
Benjamin <isj2hishhw12@gmail.com>
2025-09-17 20:45:18 +0200

    PHP Variables

---
+++ Moduli-2/index.php
@@ -0,0 +1,42 @@
+<?php 
+    
+    $school="Digital School";
+
+    echo "I learn at $school <br>";
+
+
+    $x=100;
+
+    $y=50;
+
+    echo $x + $y; echo "<br>";
+
+    echo $x - $y;echo "<br>";
+    echo $x * $y;echo "<br>";
+    echo $x / $y;echo "<br>";
+    echo $x % $y;echo "<br>";
+
+
+    $a="Digital";
+    $b="School";
+    $c=$a.$b;
+
+    
+
+    echo "$c \n";
+
+    $name_emri="Shkolla Digjitale"; echo "<br>";
+
+   echo strlen($name_emri); echo "<br>";
+
+   echo str_word_count($name_emri); echo "<br>";
+
+   $programming= "Programming is not cool";
+   echo str_replace("not","very",$programming);
+
+
+   $the_word= "Programming";echo "<br>";
+   echo strrev($the_word);
+ ?>
+
+
