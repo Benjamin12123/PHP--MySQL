dc3ab8cb415ac225b7b20fb8f0b89ebfa6a9f821
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-09-24 11:45:06 -0700

    Switch and Loops

--- Moduli-3/index.php
+++ Moduli-3/index.php
@@ -43,4 +43,57 @@
     }
     else{echo "False";}
 
+
+
+
+    $mosha=15;
+    switch($mosha){
+        case($mosha >=0 && $mosha<=18):
+        echo "You are a minor (0-18)<br>";
+        break;
+
+        case($mosha >=18 && $mosha<=25):
+        echo "Youn are a young adult";
+        break;
+
+        case($mosha >25):
+        echo "You are an adult <br>";
+        break;
+
+        default:
+        echo "Invalid Age";
+        break;
+
+
+    }
+
+    $day = "Monday";
+
+    switch($day){
+        case 'Monday':
+            echo "It is Monday <br>";
+            break;  
+             case 'Tuesday':
+            echo "It is Tuesday <br>";
+            break;
+             case 'Wednesday':
+            echo "It is Wednesday <br>";
+            break;
+             case 'Thursday':
+            echo "It is Thursday <br>";
+            break;
+             case 'Friday':
+            echo "It is Friday <br>";
+            break;
+             case 'Saturday':
+            echo "It is Saturday <br>";
+            break;      
+             case 'Sunday':
+            echo "It is Sunday <br>";
+            break;
+            default:
+            echo "Inavlid Day <br>";
+            break;
+    }
+
 ?>
